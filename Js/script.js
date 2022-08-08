37369a1f31421fe9f7908b7c43895ad6a1fdfcda
NicolasNacfur <nicolasnacfur@gmail.com>
2022-08-07 23:10:47 -0300

    Add Tortas Js

---
+++ Js/script.js
@@ -0,0 +1,3 @@
+let botonAgregar = document.getElementsByClassName("agregar")
+const divCarrito = document.querySelector("#carrito")
+const carritoSection = document.querySelector("#carritoSection")
---
+++ Js/tortas.js
@@ -0,0 +1,62 @@
+const tortas = [{
+        nombre: "Cheesecake con Frutos Rojos",
+        description: "No esta de mas aclarar que es 100% casero.. aca no usamos gelatina ni mermeladas, una vez que lo pruebes vas a sentir la diferencia 😉",
+        precio: 2500
+    },
+    {
+        nombre: "Lemon Pie",
+        description: "Frescura asegurada con esta combinación de sabores que no puede faltar en tus tardes de merienda. Que estas esperando para tener esta clásica bomba en tu heladera?",
+        precio: 1600
+    },
+    {
+        nombre: "Tarta Crema pastelera y Frutillas",
+        description: "Para estos dias de intenso calor te recomendamos que pruebes nuestra tarta de crema pastelera y frutillas. Ideal para darle un toque fresco y rico a esas meriendas con 30 grados de calor..",
+        precio: 2000
+    },
+    {
+        nombre: "Torta Brownie",
+        description: "No te olvides de encargar está bomba con mucho ddl y merengue. Podes pedirla con o sin nueces!",
+        precio: 1800
+    },
+    {
+        nombre: "Gâteau oreo",
+        description: "Su base está hecha con Oreos y café, rellena con una mousse de dulce de leche y mas oreos... Un giro totalmente distinto a lo que seria una chocotorta de oreo normal. Pecado no es comerla si no, no encargarla.",
+        precio: 2300
+    },
+    {
+        nombre: "Apple Crumble",
+        description: "Seguimos con los clásicos en Notre y a partir de hoy vas a poder encargar este delicioso crumble de manzana. Acompañalo con una bocha de helado de americana para aportarle un toque de frescura y mas placer 🤤",
+        precio: 2500
+    },
+    {
+        nombre: "Brownies",
+        description: "Estos brownies son súper húmedos por dentro y crocantes por fuera....un viaje de ida. Podes pedirlos con o sin nuez, como mas te guste!",
+        precio: 3500
+    },
+    {
+        nombre: "Tiramisu Cheesecake",
+        description: "Suave crema de queso, vainillas bañadas en café, con un toque de cacao amargo",
+        precio: 2000
+    },
+    {
+        nombre: "Dark Lemon Pie",
+        description: "Este giro de rosca al clasico Lemon Pie es algo que se tiene que quedar. Estamos muy seguros que una vez que lo pruebes, va a ser amor a primer bocado. No te quedes sin el tuyo!",
+        precio: 1600
+    },
+    {
+        nombre: "Carrot Cake",
+        description: "Date el gusto de probar algo nuevo y pedi nuestra dulce torta de zanahoria. Tiene pasas de uva, nueces y un toque de canela que le da ese extra de sabor!",
+        precio: 1400
+    },
+    {
+        nombre: "Red Velvet",
+        description: "Si alguna vez quisite comer algo tradicional de la pasteleria estadounidense, esta es tu oportunidad! Te presentamos el famoso pastel de terciopelo rojo para tu disfrute.",
+        precio: 1700
+    },
+    {
+        nombre: "Flan al Caramelo",
+        description: "Lo clasico nunca falla dicen, por eso nosotros te traemos el postre mas clasico de todos. Asi solo es una delicia pero como todos sabemos, lo mas recomendable es que los pidas con crema y ddl.",
+        precio: 1600
+    },
+
+]
